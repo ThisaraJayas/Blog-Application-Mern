b4030800dd03d8c930fd438138ea581401028440
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-10 21:21:24 +0530

    g

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
+        </div>
+        <div className="texts">
+        <h2>Winners of the Holiday Jamboree!</h2>
+        <p>I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
+        </div>
+        <div className="texts">
+        <h2>Winners of the Holiday Jamboree!</h2>
+        <p>I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
+        </div>
+        <div className="texts">
+        <h2>Winners of the Holiday Jamboree!</h2>
+        <p>I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
