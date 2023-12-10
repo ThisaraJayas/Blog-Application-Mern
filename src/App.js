466229f0bb237356b359628cf76d553aeae7d48e
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-10 22:07:14 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Post from './component/Post';
+import Header from './component/Header';
+import{Route, Routes} from 'react-router-dom'
+import Layout from './component/Layout';
+import IndexPage from './component/IndexPages';
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
-        </div>
-        <div className="texts">
-        <h2>Winners of the Holiday Jamboree!</h2>
-        <p className="info">
-          <a className="author">Thisara Jay</a>
-          <time>2023-12-10 14:45</time>
-        </p>
-        <p className="summary">I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
-        </div>
-        <div className="texts">
-        <h2>Winners of the Holiday Jamboree!</h2>
-        <p className="info">
-          <a className="author">Thisara Jay</a>
-          <time>2023-12-10 14:45</time>
-        </p>
-        <p className="summary">I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
-        </div>
-        <div className="texts">
-        <h2>Winners of the Holiday Jamboree!</h2>
-        <p className="info">
-          <a className="author">Thisara Jay</a>
-          <time>2023-12-10 14:45</time>
-        </p>
-        <p className="summary">I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
-        </div>
-      </div>
-    </main>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<IndexPage/>}/>
+          <Route path='/login' element={<div>Login</div>}/>
+        </Route>
+      </Routes>
+
   );
 }
 
---
+++ src/component/Post.js
@@ -0,0 +1,18 @@
+export default function Post(){
+    return(
+        <div className="post">
+        <div className="image">
+        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
+        </div>
+        <div className="texts">
+        <h2>Winners of the Holiday Jamboree!</h2>
+        <p className="info">
+          <a className="author">Thisara Jay</a>
+          <time>2023-12-10 14:45</time>
+        </p>
+        <p className="summary">I had no idea that Sylvia Plath wrote children's stories and I'm not sure why this surprised me so much. I purchased this book on a whim in a charity shop, out of curiosity but also because I have not really read much of Plath's work.</p>
+        </div>
+      </div>
+      
+    );
+}
