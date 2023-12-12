a5decb2063928b3f5838d97512b40dc97a488f5b
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-12 19:26:35 +0530

    comit

--- src/component/Post.js
+++ src/component/Post.js
@@ -1,15 +1,15 @@
 import {format} from 'date-fns'
 //                      grab all data from here
-export default function Post({title,summary,cover,content,createdAt}){
+export default function Post({title,summary,cover,content,createdAt,author}){
     return(
         <div className="post">
         <div className="image">
-        <img src="https://images.unsplash.com/photo-1545048702-79362596cdc9?fit=max&fm=jpg&ixid=M3wzNTY3MHwwfDF8YWxsfHx8fHx8fHx8MTcwMTk3ODkyN3w&ixlib=rb-4.0.3&q=75&w=720&utm_medium=referral&utm_source=vocal.media"></img>
+        <img src={'http://localhost:4000/'+cover}></img>
         </div>
         <div className="texts">
         <h2>{title}</h2>
         <p className="info">
-          <a className="author">Thisara Jay</a>
+          <a className="author">{author.username}</a>
           {/* to print date in time format = client-> npm install date-fns */}
           <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
         </p>
