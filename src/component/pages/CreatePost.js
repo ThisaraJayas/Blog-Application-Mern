9ca894262aee9c253b0632fcfe5e99dff13e93fb
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-12 17:29:11 +0530

    comit

--- src/component/pages/CreatePost.js
+++ src/component/pages/CreatePost.js
@@ -20,14 +20,39 @@ export default function CreatePost(){
     const [title, setTitle] = useState('')
     const [summary,setSummary] = useState('')
     const [content,setContent] = useState('')
-    
+    const [files, setFiles] = useState('')
+   async function createNewPost(ev){
+        //because we have images and stuff we are not sending as json body insted we send as form
+        const data = new FormData()
+        data.set('title',title)
+        data.set('summary',summary)
+        data.set('content',content)
+        data.set('file',files[0])
+        ev.preventDefault()
+       const response = await fetch('http://localhost:4000/post',{
+            method: 'POST',
+            body: data,
+        })
+        console.log(await response.json())
+    }
     return(
-        <form>
-            <input type="title" placeholder={'Title'}/>
-            <input type="summary" placeholder={'Summary'}/>
-            <input type="file"/>
+        <form onSubmit={createNewPost}>
+            <input type="title" placeholder={'Title'} 
+            value={title}
+            onChange={ev=>setTitle(ev.target.value)
+            }/>
+            <input type="summary" placeholder={'Summary'}
+             value={summary}
+            onChange={ev=>setSummary(ev.target.value)}
+            />
+            <input type="file" 
+            onChange={ev=>setFiles(ev.target.files)}/>
             {/* //to get textedditor like input install react-quill */}
-            <ReactQuill value={content} modules={models} formats={formats}/>
+            <ReactQuill value={content} 
+            modules={models} 
+            formats={formats}
+            onChange={newValue=>setContent(newValue)}
+            />
             <button style={{marginTop: '5px'}}>Create Post</button>
             
         </form>
