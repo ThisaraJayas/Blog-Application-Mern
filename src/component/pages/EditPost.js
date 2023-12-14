83e512fc3a4429a9304065e8ec489b32389365cb
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-14 23:11:09 +0530

    commit

--- src/component/pages/EditPost.js
+++ src/component/pages/EditPost.js
@@ -44,6 +44,7 @@ export default function EditPost(){
         data.set('title',title)
         data.set('summary',summary)
         data.set('content',content)
+        data.set('id',id)
         if(files?.[0]){
             data.set('file',files?.[0])
         }
@@ -51,6 +52,7 @@ export default function EditPost(){
         const response = await fetch('http://localhost:4000/post',{
             method: 'PUT', //put for update
             body: data,
+            credentials: 'include'
         })
         if(response.ok){
             setRedirect(true)
